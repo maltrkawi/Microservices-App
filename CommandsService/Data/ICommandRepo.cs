cd3d8819d1b006625e1383161a09baa851fdd6c5
Mohammad Altrkawi <m.altrkawi@gmail.com>
2022-08-12 17:35:49 +0300

    Models, Repos, Dtos, and AutoMapper for command service

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform platform);
+        bool PlatformExists(int platformId);
+
+        IEnumerable<Command> GetCommandsByPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+
+    }
+}
